6e2643a17721b01d7a4271c41ed909d05dd10fb9
Mahdi-Islam071004 <mohammed.islam@kainos.com>
2026-08-13 09:26:16 +0100

    progress made

--- src/models/application.ts
+++ src/models/application.ts
@@ -2,6 +2,7 @@ export interface Application {
   applicationId: number;
   applicantName: string;
   applicantEmail: string;
+  jobRoleId?: number | null;
   roleName: string;
   applicationDate: string;
   status: "pending" | "approved" | "rejected";
